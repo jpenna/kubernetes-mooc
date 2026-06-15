2b5b480b3fc8053145ab039fcef43c244f5f81f2
Juliano Penna <julianopenna@gmail.com>
2026-06-15 11:10:35 -0300

    Update push script

--- scripts/push-exercise.sh
+++ scripts/push-exercise.sh
@@ -7,18 +7,27 @@ COMMIT_MESSAGE=$2
 EXERCISE_DESCRIPTION="${3:-$COMMIT_MESSAGE}"
 
 if [[ -z "$EXERCISE_NUMBER" || -z "$COMMIT_MESSAGE" ]]; then
-  echo "Usage: $0 <exercise_number> <commit_message> [exercise_description]"
+  echo -e "\033[31mError: Usage: $0 <exercise_number> <commit_message> [exercise_description]\033[0m"
   exit 1
 fi
 
 if ! [[ "$EXERCISE_NUMBER" =~ ^[0-9]\.[0-9]$ ]]; then
-  echo "Error: Exercise number must be in the format single_digit.single_digit (e.g., 1.2)"
+  echo -e "\033[31mError: Exercise number must be in the format single_digit.single_digit (e.g., 1.2)\033[0m"
+  exit 1
+fi
+
+if ! grep -q "$EXERCISE_NUMBER" README.md; then
+  echo -e "\033[31mError: Exercise number $EXERCISE_NUMBER not found in README.md.\033[0m"
   exit 1
 fi
 
 
+echo -e "\033[32mPushing exercise $EXERCISE_NUMBER\033[0m"
+
 git add .
 git commit -m "$2"
 git push
+
+echo -e "\033[32mCreating release $EXERCISE_NUMBER\033[0m"
 
 gh release create "$EXERCISE_NUMBER" --title "$EXERCISE_NUMBER" --notes "$COMMIT_MESSAGE"
